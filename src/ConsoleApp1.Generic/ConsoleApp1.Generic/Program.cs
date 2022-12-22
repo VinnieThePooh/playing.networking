3511569a7f21355cd913857add32f6bcacc90133
Lashkevich Ryan <lashkevichrain@gmail.com>
2022-12-22 12:22:09 +0700

    nchore(): removed extern project

--- src/ConsoleApp1.Generic/ConsoleApp1.Generic/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿var obj = new LeftClass<FooDevice>();
-obj.Device = new FooDevice("foo");
-
-Console.WriteLine(obj.DeviceType);
-
-var obj2 = new LeftClass<BarDevice>();
-obj2.Device = new BarDevice("bar");
-Console.WriteLine(obj2.DeviceType);
